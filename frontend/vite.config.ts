6db6253c3e1b32bcbdd8915d097d2c4f05c2e76f
analisisapps <analisisapp.mx@aleatica.com>
2026-02-17 22:01:48 -0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../static',
+    outDir: 'dist',
     emptyOutDir: true,
     sourcemap: true
   },
